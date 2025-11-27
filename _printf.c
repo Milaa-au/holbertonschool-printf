05fe67a0ffa922b79bca05a4e54d22a35511955b
Milaa-au <mila.audu@icloud.com>
2025-11-27 08:23:48 +0000

    Ajout des commentaires Betty et correction des erreurs Betty.

--- _printf.c
+++ _printf.c
@@ -2,6 +2,20 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+* _printf - Imprime une chaîne formatée selon des spécificateurs donnés.
+* @format: Chaine de format contenant les caractères et spéci à traiter
+*
+* Description: Cette fonction parcourt la chaîne de format et imprime
+* chaque caractère. Lorsqu'un caractère '%' est rencontré, elle appelle
+* la fonction get_func afin de traiter le spécificateur correspondant
+* et d’imprimer la valeur adaptée. Si le spécificateur n’est pas valide,
+* le caractère '%' ainsi que le caractère suivant sont imprimés tels quels.
+*
+* Return: Le nombre total de caractères imprimés. Retourne -1 si format
+* est NULL.
+*/
+
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -18,26 +32,25 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			 putchar(format[i]);
+			putchar(format[i]);
 			count++;
 		}
 		else
 		{
-			
 			temp = get_func(format[i], args);
 
-            if (temp == -1)
-            {
-                count += putchar('%');
-                count += putchar(format[i]);
-            }
-            else
-            {
-                count += temp;
-            }
-        }
-        i++;
-    }
+			if (temp == -1)
+			{
+				count += putchar('%');
+				count += putchar(format[i]);
+			}
+			else
+			{
+				count += temp;
+			}
+		}
+		i++;
+	}
 
 	va_end(args);
 	return (count);
